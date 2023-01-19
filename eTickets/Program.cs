2e239cc81f5f360e8b284746772ea49e0f6153ff
erhantopcu <erhn.topcu@hotmail.com>
2023-01-19 12:00:13 +0300

    DbContext

--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -1,9 +1,69 @@
+using eTickets.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+//DbContext configuration
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionStrings")));
+
+////Services configuration
+//builder.Services.AddScoped<IActorsService, ActorsService>();
+//builder.Services.AddScoped<IProducersService, ProducersService>();
+//builder.Services.AddScoped<ICinemasService, CinemasService>();
+//builder.Services.AddScoped<IMoviesService, MoviesService>();
+//builder.Services.AddScoped<IOrdersService, OrdersService>();
+
+//builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+//builder.Services.AddScoped(sc => ShoppingCart.GetShoppingCart(sc));
+
+////Authentication and authorization
+//builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
+//builder.Services.AddMemoryCache();
+//builder.Services.AddSession();
+//builder.Services.AddAuthentication(options =>
+//{
+//    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+//});
+
+//builder.Services.AddControllersWithViews();
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseSession();
+
+//Authentication & Authorization
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Movies}/{action=Index}/{id?}");
+});
+
+//Seed database
+AppDbInitializer.Seed(app);
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
